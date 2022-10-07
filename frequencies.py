cde4c2723e6b13ae8bf87ae9de06f6a5c0e920e8
runxin.shao <runxin.shao@kcl.ac.uk>
2022-10-07 18:45:20 +0100

    frequencies counter exercise
    a

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for i in items:
+        i = str(i)
+        if i in frequencies:
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
     return frequencies
